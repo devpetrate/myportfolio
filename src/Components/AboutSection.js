96945dc57c207df46f11463b131a6e5fc3133425
Petrate <128822730+devpetrate@users.noreply.github.com>
2023-08-26 17:25:33 +0100

    Portfolio 0.0.4

--- src/Components/AboutSection.js
+++ src/Components/AboutSection.js
@@ -5,8 +5,18 @@ function AboutSection() {
     return (
         <div className='aboutwrapper'>
             <h1><span class="underline">Ab</span>out</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum officiis deserunt consequatur sit dignissimos modi distinctio possimus
-                voluptates, saepe eligendi illo nostrum voluptas repellendus autem iusto error incidunt facere. Voluptatibus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum officiis deserunt consequatur sit dignissimos modi distinctio possimus voluptates, saepe eligendi illo nostrum voluptas repellendus autem iusto error incidunt facere. Voluptatibus.</p>
+            <p>I'm Peter Adelakun Ademurewa, a dedicated learner who transitioned into the tech industry
+                with a background in Physiology. Through intensive self-study and hands-on practice,
+                I've earned certifications in Responsive Web Design, JavaScript Algorithms and Data Structures,
+                and Front End Libraries from freeCodeCamp, as well as a Front-End Developer certification through
+                Meta - Coursera. </p>
+            <p>My GitHub repository showcases diverse projects where I've translated my learning
+                into tangible web solutions, spanning interactive interfaces to responsive layouts. As I continue to
+                learn, build, and grow, I am excited about the potential to create meaningful and impactful
+                technological solutions. With a strong foundation in responsive web design, JavaScript, and
+                front-end libraries, I am poised to embrace new challenges and contribute positively to the
+                dynamic world of technology. I'm enthusiastic about the opportunity to collaborate with experienced
+                developers, bringing fresh insights and a strong foundation in front-end development to future projects.</p>
             <section>
                 <a href="https://drive.google.com/file/d/1xQ7Pt8OFnoEvjiOYX6TkNalhVdgwJ95u/view?usp=sharing"
                     target="_blank"
--- src/Components/CerificationSection.js
+++ src/Components/CerificationSection.js
@@ -16,50 +16,99 @@ function CertificationSection() {
             <h1><span class="underline">Ce</span>rtification</h1>
             <section className='portfoliojobswrapper'>
                 <div className='portwordswrapper'>
-                    <h2>Meta Front-End Developer</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <h2><a href="https://coursera.org/share/d5835975b0e7481499ac501328421c23"
+                        target="_blank"
+                        rel="noreferrer">Meta Front-End Developer</a></h2>
+                    <p>Completed Meta's Front-End Developer Certificate,
+                        mastering React, Version Control, UX/UI Design principles,
+                        and gaining in-depth knowledge of HTML, CSS, and JavaScript.
+                        This comprehensive learning equips me to craft dynamic interfaces
+                        and excel in modern web development.</p>
                     <div className='portlogowrapper'>
-                        <img src={weblogo} alt="gitlogo" />
-                        <p>Certificate Page</p>
+                        <a href="https://coursera.org/share/d5835975b0e7481499ac501328421c23"
+                            target="_blank"
+                            rel="noreferrer"><img src={weblogo} alt="gitlogo" /></a>
+                        <p><a href="https://coursera.org/share/d5835975b0e7481499ac501328421c23"
+                            target="_blank"
+                            rel="noreferrer">Certificate Page</a></p>
                     </div>
                 </div>
-                <img className='workpic' src={cert1} alt="" />
+                <a href="https://coursera.org/share/d5835975b0e7481499ac501328421c23"
+                    target="_blank"
+                    rel="noreferrer"><img className='workpic' src={cert1} alt="" /></a>
             </section>
             <section className='portfoliojobswrapper'>
                 <div className='portwordswrapper'>
-                    <h2>Front-End Development Libraries by freeCodeCamp</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <h2><a href="https://www.freecodecamp.org/certification/Petrate/front-end-development-libraries"
+                        target="_blank"
+                        rel="noreferrer">Front-End Development Libraries by freeCodeCamp</a></h2>
+                    <p>Completed the Front End Development Libraries curriculum
+                        on freeCodeCamp, where I immersed myself in mastering essential
+                        tools and technologies. From harnessing the power of Bootstrap,
+                        jQuery, and SASS to building dynamic interfaces with React and
+                        Redux, I've acquired a versatile skill set that enables me to
+                        create modern, feature-rich web applications.</p>
                     <div className='portlogowrapper'>
 
-                        <img src={weblogo} alt="gitlogo" />
-                        <p>Certificate Page</p>
+                        <a href="https://www.freecodecamp.org/certification/Petrate/front-end-development-libraries"
+                            target="_blank"
+                            rel="noreferrer"><img src={weblogo} alt="gitlogo" /></a>
+                        <p><a href="https://www.freecodecamp.org/certification/Petrate/front-end-development-libraries"
+                            target="_blank"
+                            rel="noreferrer">Certificate Page</a></p>
                     </div>
                 </div>
-                <img className='workpic' src={cert2} alt="" />
+                <a href="https://www.freecodecamp.org/certification/Petrate/front-end-development-libraries"
+                    target="_blank"
+                    rel="noreferrer"><img className='workpic' src={cert2} alt="" /></a>
             </section>
             <section className='portfoliojobswrapper'>
                 <div className='portwordswrapper'>
-                    <h2>JavaScript Algorithms and Data Structures by freeCodeCamp </h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <h2><a href="https://www.freecodecamp.org/certification/Petrate/javascript-algorithms-and-data-structures"
+                        target="_blank"
+                        rel="noreferrer">JavaScript Algorithms and Data Structures by freeCodeCamp </a></h2>
+                    <p>Completing the JavaScript Algorithms and Data Structures curriculum on freeCodeCamp
+                        has been a transformative journey. From mastering the basics of JavaScript and ES6
+                        to tackling complex challenges in Regular Expressions, Data Structures, and Algorithms,
+                        I've honed my problem-solving skills and deepened my understanding of programming principles,
+                        equipping me with the expertise to solve intricate coding challenges and build efficient, scalable applications.</p>
                     <div className='portlogowrapper'>
 
-                        <img src={weblogo} alt="gitlogo" />
-                        <p>Certificate Page</p>
+                        <a href="https://www.freecodecamp.org/certification/Petrate/javascript-algorithms-and-data-structures"
+                            target="_blank"
+                            rel="noreferrer"><img src={weblogo} alt="gitlogo" /></a>
+                        <p><a href="https://www.freecodecamp.org/certification/Petrate/javascript-algorithms-and-data-structures"
+                            target="_blank"
+                            rel="noreferrer">Certificate Page</a></p>
                     </div>
                 </div>
-                <img className='workpic' src={cert3} alt="" />
+                <a href="https://www.freecodecamp.org/certification/Petrate/javascript-algorithms-and-data-structures"
+                    target="_blank"
+                    rel="noreferrer"><img className='workpic' src={cert3} alt="" /></a>
             </section>
             <section className='portfoliojobswrapper'>
                 <div className='portwordswrapper'>
-                    <h2>Responsive Web Design by freeCodeCamp </h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <h2><a href="https://www.freecodecamp.org/certification/Petrate/responsive-web-design"
+                        target="_blank"
+                        rel="noreferrer">Responsive Web Design by freeCodeCamp </a></h2>
+                    <p>Embarking on the Responsive Web Design curriculum on freeCodeCamp has been an immersive experience.
+                        Starting with learning HTML by building a Cat Photo App and progressing through various milestones,
+                        including CSS Transforms by building a Penguin, I've cultivated a solid foundation in Basic CSS,
+                        HTML Forms, Flexbox, CSS Animation, and more. This journey has equipped me with the skills needed
+                        to craft engaging, responsive web experiences that seamlessly adapt to different devices and screen sizes.</p>
                     <div className='portlogowrapper'>
 
-                        <img src={weblogo} alt="gitlogo" />
-                        <p>Certificate Page</p>
+                        <a href="https://www.freecodecamp.org/certification/Petrate/responsive-web-design"
+                            target="_blank"
+                            rel="noreferrer"><img src={weblogo} alt="gitlogo" /></a>
+                        <p><a href="https://www.freecodecamp.org/certification/Petrate/responsive-web-design"
+                            target="_blank"
+                            rel="noreferrer">Certificate Page</a></p>
                     </div>
                 </div>
-                <img className='workpic' src={cert4} alt="" />
+                <a href="https://www.freecodecamp.org/certification/Petrate/responsive-web-design"
+                    target="_blank"
+                    rel="noreferrer"><img className='workpic' src={cert4} alt="" /></a>
             </section>
         </div>
     )
